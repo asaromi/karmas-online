f9cac7b6290fedc37ab13b125d2ea10830f1b8cd
asaromi <a.sabililromi@gmail.com>
2020-09-07 06:39:27 +0700

    Controller : method print to download student card

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use AsalNgoding\Fpdf\PDF_Code128;
+use Illuminate\Support\Facades\Storage;
 
 class HomeController extends Controller
 {
@@ -30,5 +32,16 @@ class HomeController extends Controller
         $faculty = $user->department->faculty->name;
 
         return view('home', compact(['user', 'degree', 'department', 'faculty']));
+    }
+
+    public function print(){
+        $pdf = new PDF_Code128('P','mm',array(85.5,54));
+        $pdf->AddPage();
+
+        $pdf->Image('storage/img/foto.jpg', 70, 59, 17, 22);
+
+        $pdf->AddFont('Arial Narrow','','ARIALNB.php');
+        $pdf->SetFont('Arial','B',16);
+        dd($pdf->Output('','anu'));
     }
 }
