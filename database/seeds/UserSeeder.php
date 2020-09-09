f3908d5490077efed473c94d69e1632d050bcb78
asaromi <a.sabililromi@gmail.com>
2020-09-09 22:19:53 +0700

    Seeders : add seed for faculty

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -11,10 +11,123 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('faculties')->insert([
-            'id' => 9,
+        DB::table('faculties')->insert([ //1
+            'code' => 'C',
+            'name' => 'Seni Rupa dan Desain',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('faculties')->insert([ //2
+            'code' => 'D',
+            'name' => 'Ilmu Sosial dan Ilmu Politik',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('faculties')->insert([ //3
+            'code' => 'E',
+            'name' => 'Hukum',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('faculties')->insert([ //4
+            'code' => 'F',
+            'name' => 'Ekonomi dan Bisnis',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('faculties')->insert([ //5
+            'code' => 'G',
+            'name' => 'Kedokteran',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('faculties')->insert([ //6
+            'code' => 'H',
+            'name' => 'Pertanian',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('faculties')->insert([ //7
+            'code' => 'I',
+            'name' => 'Teknik',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        
+        DB::table('faculties')->insert([ //8
+            'code' => 'K',
+            'name' => 'FKIP',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('faculties')->insert([ //9
             'code' => 'M',
             'name' => 'Matematika dan Ilmu Pengetahuan Alam',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('faculties')->insert([ //10
+            'code' => 'R',
+            'name' => 'Kedokteran',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('faculties')->insert([ //11
+            'code' => 'A',
+            'name' => 'Pascasarjana',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('faculties')->insert([ //12
+            'code' => 'S',
+            'name' => 'Pascasarjana',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('faculties')->insert([ //13
+            'code' => 'T',
+            'name' => 'Pascasarjana',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('faculties')->insert([ //14
+            'code' => 'X',
+            'name' => 'FKIP',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('faculties')->insert([ //15
+            'code' => 'P',
+            'name' => 'FKIP',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('faculties')->insert([
+            'id' => 20,
+            'code' => 'B',
+            'name' => 'Ilmu Budaya',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('faculties')->insert([
+            'id' => 21,
+            'code' => 'O',
+            'name' => 'Keolahragaan',
             'created_at' => date("Y-m-d H:i:s"),
             'updated_at' => date("Y-m-d H:i:s")
         ]);
